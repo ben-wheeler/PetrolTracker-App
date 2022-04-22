ad09fe2a3677bf625e2e6383e594f518dddf7bd2
Ben <92675290+ben-wheeler@users.noreply.github.com>
2022-04-22 14:32:56 +0930

    Create AddView.swift

---
+++ PetrolTracker/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  PetrolTracker
+//
+//  Created by Ben Wheeler on 22/4/2022.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
