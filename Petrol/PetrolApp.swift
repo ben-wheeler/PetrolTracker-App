43a3967162ff34bb2544e0deb402bb5210f2e6eb
Ben <92675290+ben-wheeler@users.noreply.github.com>
2022-04-22 12:10:18 +0930

    Initial Commit

---
+++ Petrol/PetrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetrolApp.swift
+//  Petrol
+//
+//  Created by Ben Wheeler on 22/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct PetrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
