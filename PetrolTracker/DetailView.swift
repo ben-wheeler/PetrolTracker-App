8723eabde1b2a2ce9c55a2da1be359cf220f9e2b
Ben <92675290+ben-wheeler@users.noreply.github.com>
2023-04-06 13:46:16 +0930

    Updated to work with core data

--- PetrolTracker/DetailView.swift
+++ PetrolTracker/DetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
      
-    var item: Entry
+    var item: Log
     
         var body: some View {
         NavigationView{
@@ -18,35 +18,38 @@ struct DetailView: View {
                     Text("Distance Travelled")
                         .bold()
                     Spacer()
-                    Text(item.distance, format: .measurement(width: .abbreviated))
+                    Text(String(item.distance))
+//                    Text(item.distance, format: .measurement(width: .abbreviated))
                 }
                 HStack{
                     Text("Petrol Used")
                         .bold()
                     Spacer()
-                    Text(item.fuel, format: .measurement(width: .abbreviated))
+                    Text(String(item.fuel))
+//                    Text(item.fuel, format: .measurement(width: .abbreviated))
                 }
                 HStack{
                     Text("Efficiency")
                         .bold()
                     Spacer()
-                    Text(item.efficiency, format: .measurement(width: .abbreviated))
+                    Text(String(item.efficiency))
+//                    Text(item.efficiency, format: .measurement(width: .abbreviated))
                 }
             }
-            .navigationTitle(Text(item.created, format: .dateTime.day().month().year()))
+            .navigationTitle(Text(item.created ?? Date(), format: .dateTime.day().month().year()))
         }
     }
 }
 
-struct DetailView_Previews: PreviewProvider {
-    static let d = Measurement(value: 216.5, unit: UnitLength.kilometers)
-    static let v = Measurement(value: 65468789645, unit: UnitVolume.liters)
-    static let e = Measurement(value: 9.8, unit: UnitFuelEfficiency.litersPer100Kilometers)
-
-    static let test = Entry(created: Date.now, distance: d, fuel: v, efficiency: e)
-    
-    static var previews: some View {
-        DetailView(item: test)
-//            .environment(\.locale, .init(identifier: "en_AU"))
-    }
-}
+//struct DetailView_Previews: PreviewProvider {
+////    static let d = Measurement(value: 216.5, unit: UnitLength.kilometers)
+////    static let v = Measurement(value: 65468789645, unit: UnitVolume.liters)
+////    static let e = Measurement(value: 9.8, unit: UnitFuelEfficiency.litersPer100Kilometers)
+////
+////    static let test = Entry(created: Date.now, distance: d, fuel: v, efficiency: e)
+//
+//    static var previews: some View {
+//        DetailView()
+////            .environment(\.locale, .init(identifier: "en_AU"))
+//    }
+//}
